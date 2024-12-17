5e70cff1ea27a5141e10a667e6e8bd7a56b006ea
KoJaem <woalswhwh@naver.com>
2024-12-17 18:53:39 +0900

    chore: 빌드오류 테스트를 위한 빌드오류 코드 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/Button";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
+  빌드오류 테스트
   return (
     <>
       <button className="p-[40px] rounded-lg bg-primary text-white dark:bg-white dark:text-primary">
