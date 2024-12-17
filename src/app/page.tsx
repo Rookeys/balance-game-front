c223be3ae864200f12643f9615b23f43281caead
KoJaem <woalswhwh@naver.com>
2024-12-17 19:12:41 +0900

    chore: 테스트용 빌드오류 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/Button";
 import ThemeToggle from "@/components/ThemeToggle";
 
 export default function Home() {
-  build error test
   return (
     <>
       <button className="p-[40px] rounded-lg bg-primary text-white dark:bg-white dark:text-primary">
