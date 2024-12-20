a99a3a6e8d19db4a874a07301d0a698d0c4b9646
KoJaem <woalswhwh@naver.com>
2024-12-20 23:46:14 +0900

    chore: Information UI 수정 및 title 추가

--- src/components/gamethumbnail/_components/Information.tsx
+++ src/components/gamethumbnail/_components/Information.tsx
@@ -5,10 +5,16 @@ interface Params {
 }
 export function Information({ gameTitle, gameDescription, creator }: Params) {
   return (
-    <article className="flex flex-col gap-[8px]">
-      <p className="text-md font-semibold truncate">{gameTitle}</p>
-      <p className="text-base line-clamp-2">{gameDescription}</p>
-      <p className="text-sm text-gray-60 dark:text-gray-30">제작자: {creator}</p>
+    <article className="flex flex-col gap-[4px]">
+      <p className="text-md font-semibold truncate" title={gameTitle}>
+        {gameTitle}
+      </p>
+      <p className="text-base line-clamp-2" title={gameDescription}>
+        {gameDescription}
+      </p>
+      <p className="text-sm text-gray-60 dark:text-gray-30" title={creator}>
+        제작자: {creator}
+      </p>
     </article>
   )
 }
