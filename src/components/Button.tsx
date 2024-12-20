73dcf39518972d263e78c88e4590e6dc0bc77c5a
KoJaem <woalswhwh@naver.com>
2024-12-20 23:43:36 +0900

    feat: 공용 Button 컴포넌트 구현 및 적용

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+"use client"
+import { cn } from "@/utils/cn"
+import { Slot } from "@radix-ui/react-slot"
+import * as React from "react"
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  asChild?: boolean
+  className?: string
+}
+
+export const Button: React.FC<ButtonProps> = ({ asChild = false, className, children, ...props }) => {
+  const Comp = asChild ? Slot : "button"
+  const baseClassName = "inline-flex items-center justify-center"
+
+  return (
+    <Comp className={cn(baseClassName, className)} {...props}>
+      {children}
+    </Comp>
+  )
+}
--- src/components/gamethumbnail/_components/ActionButtons.tsx
+++ src/components/gamethumbnail/_components/ActionButtons.tsx
@@ -1,16 +1,17 @@
+import { Button } from "@/components/Button"
 
 export function ActionButtons() {
   return (
     <article className="flex flex-wrap">
-      <button className="bg-primary hover:bg-primary-60 text-light px-4 py-2 rounded-xsm text-sm dark:bg-primary-70 dark:hover:bg-primary-80">
+      <Button className="bg-primary hover:bg-primary-60 text-light px-4 py-2 rounded-xsm text-sm dark:bg-primary-70 dark:hover:bg-primary-80">
         게임시작
-      </button>
-      <button className="bg-gray-10 hover:bg-gray-20 text-dark-30 px-4 py-2 rounded-xsm text-sm dark:bg-gray-70 dark:hover:bg-gray-80 dark:text-light">
+      </Button>
+      <Button className="bg-gray-10 hover:bg-gray-20 text-dark-30 px-4 py-2 rounded-xsm text-sm dark:bg-gray-70 dark:hover:bg-gray-80 dark:text-light">
         🔥 랭킹확인
-      </button>
-      <button className="bg-blue hover:bg-blue-60 text-light px-4 py-2 rounded-xsm text-sm dark:bg-blue-70 dark:hover:bg-blue-80">
+      </Button>
+      <Button className="bg-blue hover:bg-blue-60 text-light px-4 py-2 rounded-xsm text-sm dark:bg-blue-70 dark:hover:bg-blue-80">
         공유하기
-      </button>
+      </Button>
     </article>
   )
 }
