c1b42a8e30dba3f1f2747aca0aff5337f0f3b170
KoJaem <woalswhwh@naver.com>
2024-12-22 04:20:29 +0900

    chore: Header 에서 사용하는 button 공용컴포넌트 button 으로 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { Gamepad2, UserRound } from "lucide-react"
 import { useRouter } from "next/navigation"
+import { Button } from "./Button"
 import Logo from "./Logo"
 import ThemeToggle from "./ThemeToggle"
 
@@ -11,15 +12,15 @@ export default function Header() {
     <header className="flex justify-between items-center px-[12px] py-[8px] bg-light dark:bg-dark-night border-b border-gray-20 dark:border-gray-70">
       <section className="flex gap-[8px]">
         <Logo onClick={() => router.push("/")} aria-label="홈으로 가기" />
-        <button onClick={() => router.push("/game-create")} aria-label="game-create">
+        <Button onClick={() => router.push("/game-create")} aria-label="game-create">
           <Gamepad2 size={40} />
-        </button>
+        </Button>
       </section>
       <section className="flex gap-[8px]">
         <ThemeToggle />
-        <button onClick={() => router.push("/my-page")} aria-label="my-page">
+        <Button onClick={() => router.push("/my-page")} aria-label="my-page">
           <UserRound size={40} />
-        </button>
+        </Button>
       </section>
     </header>
   )
