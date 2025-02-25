a109207ceb1b7850bfd34c11e9fb4a25dd5c6890
KoJaem <woalswhwh@naver.com>
2025-02-26 07:34:02 +0900

    chore: 에러페이지 추가

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { signOut } from "next-auth/react"
+
+export default function ErrorPage() {
+  return (
+    <section className="flex flex-col gap-[20px]">
+      에러가 발생했습니다.
+      <button onClick={() => signOut({ callbackUrl: "/" })}>돌아가기</button>
+    </section>
+  )
+}
