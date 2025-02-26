67a923357a64e50161504356af491f1bbf119812
KoJaem <woalswhwh@naver.com>
2025-02-27 04:14:43 +0900

    fix: NextPage 호출을 두번씩 하는 현상 수정 #26

--- src/app/(routes)/my-page/_components/InfiniteScrollClient.tsx
+++ src/app/(routes)/my-page/_components/InfiniteScrollClient.tsx
@@ -2,14 +2,10 @@
 
 import { useGetMyGameListInfinite } from "@/api/orval/client/user-profile-controller/user-profile-controller"
 import { GetMyGameListSortType } from "@/api/orval/model/getMyGameListSortType"
-import { useSession } from "next-auth/react"
 import { useEffect } from "react"
 import { useInView } from "react-intersection-observer"
 
 export default function InfiniteScrollClient() {
-  const { data: session } = useSession()
-  console.log("session", session)
-
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = useGetMyGameListInfinite(
     { sortType: GetMyGameListSortType.idDesc },
     {
@@ -39,7 +35,9 @@ export default function InfiniteScrollClient() {
     <section className="relative flex flex-col gap-[80px]">
       {/* {data?.pages.map((page) => page.content?.map((game) => <p key={game.roomId}>{game.title}</p>))} */}
       {data?.pages.flatMap((page) => page.content || []).map((game) => <p key={game.roomId}>{game.title}</p>)}
-      <div ref={ref} className="pointer-events-none absolute bottom-[400px] h-[4px] w-full opacity-0" />
+      {!isFetchingNextPage && (
+        <div ref={ref} className="pointer-events-none absolute bottom-[400px] h-[4px] w-full opacity-0" />
+      )}
       <section>
         {isFetchingNextPage ? (
           <p>로딩중...</p>
