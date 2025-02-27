5101c3fed3e5c69591f8e76460bc86020e1b2516
KoJaem <woalswhwh@naver.com>
2025-02-27 21:26:00 +0900

    chore: 라우트 경로수정 등 짜잘한 수정

---
+++ src/app/(routes)/game-create/[id]/edit/page.tsx
@@ -0,0 +1,3 @@
+export default function GameEditPage() {
+  return <section>page</section>
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,24 @@
-"use client"
-
 import { Gamepad2, UserRound } from "lucide-react"
-import { useRouter } from "next/navigation"
-import { Button } from "./Button"
+import Link from "next/link"
 import Logo from "./Logo"
 import ThemeToggle from "./ThemeToggle"
 
 export default function Header() {
-  const router = useRouter()
-
   return (
     <header className="flex items-center justify-between border-b border-gray-20 bg-light px-[12px] py-[8px] dark:border-gray-70 dark:bg-dark-night">
       <section className="flex gap-[8px]">
-        <Logo onClick={() => router.push("/")} aria-label="홈으로 가기" />
-        <Button variant="custom" onClick={() => router.push("/game-create")} aria-label="game-create">
+        <Link href={"/"}>
+          <Logo />
+        </Link>
+        <Link href={"/game-create/new"} aria-label="game-create">
           <Gamepad2 size={40} />
-        </Button>
+        </Link>
       </section>
       <section className="flex gap-[8px]">
         <ThemeToggle />
-        <Button variant="custom" onClick={() => router.push("/my-page")} aria-label="my-page">
+        <Link href={"/my-page"} aria-label="my-page">
           <UserRound size={40} />
-        </Button>
+        </Link>
       </section>
     </header>
   )
