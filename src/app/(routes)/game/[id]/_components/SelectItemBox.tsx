c24fbecc78bfd65a91e34ba34e4783c6cfd16c46
KoJaem <woalswhwh@naver.com>
2025-03-02 03:06:55 +0900

    chore: description 가운데로 오도록 수정

--- src/app/(routes)/game/[id]/_components/SelectItemBox.tsx
+++ src/app/(routes)/game/[id]/_components/SelectItemBox.tsx
@@ -44,19 +44,21 @@ export default function SelectItemBox({
         }}
         onAnimationComplete={handleAnimationComplete}
       >
-        <ImageRatio
-          src={url}
-          alt={`${title}-thumbnail`}
-          ratio={4 / 3}
-          fill
-          className="cursor-pointer"
-          onClick={() => handleSelectItem(id)}
-        />
-        {title && (
-          <article className="pointer-events-none absolute start-[50%] top-[30%] max-w-full translate-x-[-50%] translate-y-[-50%] rounded-xsm bg-dark/30 px-4 py-2 sm:top-[50%]">
-            <p className="break-words text-white">{title}</p>
-          </article>
-        )}
+        <div className="relative">
+          <ImageRatio
+            src={url}
+            alt={`${title}-thumbnail`}
+            ratio={4 / 3}
+            fill
+            className="cursor-pointer"
+            onClick={() => handleSelectItem(id)}
+          />
+          {title && (
+            <article className="pointer-events-none absolute start-[50%] top-[50%] max-w-full translate-x-[-50%] translate-y-[-50%] rounded-xsm bg-dark/50 px-4 py-2">
+              <p className="break-words text-white">{title}</p>
+            </article>
+          )}
+        </div>
         <Button className="w-full cursor-default bg-primary-10 text-md opacity-0">선택하는 버튼</Button>
       </motion.div>
     )
@@ -73,12 +75,14 @@ export default function SelectItemBox({
       }}
       onAnimationComplete={handleAnimationComplete}
     >
-      <YoutubeRatio url={url} ratio={4 / 3} />
-      {title && (
-        <article className="pointer-events-none absolute start-[50%] top-[30%] max-w-full translate-x-[-50%] translate-y-[-50%] rounded-xsm bg-dark/30 px-4 py-2 sm:top-[50%]">
-          <p className="break-words text-white">{title}</p>
-        </article>
-      )}
+      <div className="relative">
+        <YoutubeRatio url={url} ratio={4 / 3} />
+        {title && (
+          <article className="pointer-events-none absolute start-[50%] top-[50%] max-w-full translate-x-[-50%] translate-y-[-50%] rounded-xsm bg-dark/50 px-4 py-2">
+            <p className="break-words text-white">{title}</p>
+          </article>
+        )}
+      </div>
       <Button className="w-full bg-primary-10 text-md" onClick={() => handleSelectItem(id)}>
         선택하는 버튼
       </Button>
