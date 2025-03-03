f879052d794540e893198514f201b18a21068a9e
KoJaem <woalswhwh@naver.com>
2025-03-03 17:17:25 +0900

    chore: orval 적용

--- src/api/orval/model/gameListResponse.ts
+++ src/api/orval/model/gameListResponse.ts
@@ -3,6 +3,8 @@ import type { GameListSelectionResponse } from "./gameListSelectionResponse"
 export interface GameListResponse {
   /** 게임방 ID */
   roomId?: number
+  /** 제작자 닉네임 */
+  nickname?: string
   /** 게임 타이틀 */
   title?: string
   /** 설명 */
