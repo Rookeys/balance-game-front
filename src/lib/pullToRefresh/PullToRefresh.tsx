61f11617f485a9f4b7d34a371fcee3f6f7661bc3
KoJaem <woalswhwh@naver.com>
2025-03-05 21:44:24 +0900

    chore: 불필요한 주석 제거

--- src/lib/pullToRefresh/PullToRefresh.tsx
+++ src/lib/pullToRefresh/PullToRefresh.tsx
@@ -33,36 +33,3 @@ const PullToRefresh = ({
 }
 
 export default PullToRefresh
-
-// "use client"
-// import Spinner from "@/components/Spinner"
-// import { ReactNode } from "react"
-// import dynamic from "next/dynamic"
-
-// const PullToRefreshLogic = dynamic(() => import("@/lib/pullToRefresh/PullToRefreshLogic"), { ssr: false })
-
-// interface PullToRefreshProps {
-//   children: ReactNode
-//   onRefresh?: () => Promise<void>
-//   maxDistance?: number
-//   loadingComponent?: ReactNode
-// }
-
-// const PullToRefresh = ({
-//   children,
-//   maxDistance = 50,
-//   onRefresh,
-//   loadingComponent = <Spinner />
-// }: PullToRefreshProps) => {
-//   if (typeof window !== "undefined" && window.ReactNativeWebView != null) {
-//     return (
-//       <PullToRefreshLogic maxDistance={maxDistance} onRefresh={onRefresh} loadingComponent={loadingComponent}>
-//         {children}
-//       </PullToRefreshLogic>
-//     )
-//   }
-
-//   return <>{children}</>
-// }
-
-// export default PullToRefresh
