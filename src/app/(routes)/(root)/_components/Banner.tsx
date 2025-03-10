3bbae1caadc3ba8ebd736202f7a5e75ad694ada6
KoJaem <woalswhwh@naver.com>
2025-03-10 18:24:09 +0900

    chore: Pagination 코드 리팩토링 및 반응형 추가 #36

--- src/app/(routes)/(root)/_components/Banner.tsx
+++ src/app/(routes)/(root)/_components/Banner.tsx
@@ -6,7 +6,7 @@ import { Pagination } from "swiper/modules"
 
 export default function Banner() {
   return (
-    <div className="h-full w-full px-[16px] md:px-[24px] lg:px-0">
+    <div className="h-full w-full">
       <Swiper
         className="relative h-[400px] w-full bg-gray-30"
         modules={[Pagination]}
@@ -22,16 +22,18 @@ export default function Banner() {
         }}
       >
         {Array.from({ length: 5 }, (_, index) => (
-          <SwiperSlide className="w-full" key={index}>
-            <section className="mx-auto flex h-full w-full max-w-[1200px] bg-red-10 pb-[112px] pt-[88px]">
+          <SwiperSlide className="w-full px-[16px] md:px-[24px] lg:px-0" key={index}>
+            <section className="flex h-full w-full max-w-[1200px] bg-red-10 pb-[112px] pt-[88px] lg:mx-auto">
               <article>{index + 1}</article>
             </section>
           </SwiperSlide>
         ))}
-        <div
-          id="banner-slider-pagination"
-          className="absolute bottom-[40px] start-[calc(50%+600px)] z-[1] h-[32px] min-w-[60px] translate-x-[-100%] whitespace-nowrap rounded-full bg-blue-10 px-[12px] py-[4px] text-center"
-        />
+        <div className="relative mx-auto max-w-[1200px]">
+          <div
+            id="banner-slider-pagination"
+            className="absolute bottom-[40px] end-[16px] z-[1] h-[32px] min-w-[60px] whitespace-nowrap rounded-full bg-blue-10 px-[12px] py-[4px] text-center md:end-[24px] lg:end-0"
+          />
+        </div>
       </Swiper>
     </div>
   )
