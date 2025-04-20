32fb8fe478f76d841bdc45e14e1038444f763dd6
KoJaem <woalswhwh@naver.com>
2025-04-20 23:27:52 +0900

    chore: detail Page prefetch fetch 기반으로 수정 #46

--- src/app/(routes)/(ssg)/game/[id]/page.tsx
+++ src/app/(routes)/(ssg)/game/[id]/page.tsx
@@ -1,8 +1,8 @@
-import { prefetchGetGameStatus } from "@/api/orval/server/main-page-controller/main-page-controller"
-import { OrvalPrefetchBoundary } from "@/lib/providers/OrvalPrefetchBoundary"
+import { getGetGameStatusQueryKey } from "@/api/orval/client/main-page-controller/main-page-controller"
+import GameDetailPageClient from "@/app/(routes)/(ssr)/game/[id]/_components/GameDetailPageClient"
+import { FetchPrefetchBoundary } from "@/lib/providers/FetchPrefetchBoundary"
 import { QueryClient } from "@tanstack/react-query"
 import { notFound } from "next/navigation"
-import GameDetailPageClient from "@/app/(routes)/(ssr)/game/[id]/_components/GameDetailPageClient"
 
 interface Params {
   id: string
@@ -13,23 +13,25 @@ interface GameDetailPageProps {
 }
 
 export const revalidate = 300
-
 export const dynamic = "force-static"
 
 export default async function GameDetailPage({ params }: GameDetailPageProps) {
   const { id } = await params
   const queryClient = new QueryClient()
+
   return (
     <section className="mt-[20px] flex flex-col items-center gap-[28px] px-[16px] md:mt-[40px] md:gap-[40px] md:px-[24px] lg:px-0">
-      {/* <Suspense fallback={<Skeleton className="h-[600px] w-full" />}> */}
-      <OrvalPrefetchBoundary
-        prefetchActions={prefetchGetGameStatus(queryClient, Number(id))}
+      <FetchPrefetchBoundary
+        prefetchActions={fetch(`${process.env.NEXT_PUBLIC_API_ROOT}/api/v1/games/${id}`, {
+          cache: "force-cache",
+          next: { revalidate: 300 }
+        })}
+        queryKey={getGetGameStatusQueryKey(Number(id))}
         queryClient={queryClient}
         onError={notFound}
       >
         <GameDetailPageClient />
-      </OrvalPrefetchBoundary>
-      {/* </Suspense> */}
+      </FetchPrefetchBoundary>
     </section>
   )
 }
