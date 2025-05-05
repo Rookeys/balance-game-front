ce3db423c670726af14a587d9055f17d6c8e2278
KoJaem <woalswhwh@naver.com>
2025-05-05 23:43:00 +0900

    chore: sr-only 타이틀 추가

--- src/components/slider/SliderTitle.tsx
+++ src/components/slider/SliderTitle.tsx
@@ -11,7 +11,7 @@ export default function SliderTitle({ title, prevElId, nextElId, updateTime }: P
   return (
     <section className="flex h-[40px] items-center justify-between">
       <article className="flex flex-col">
-        <p className="font-sb-aggro-medium text-heading-4 text-label-normal md:text-heading-3">{title}</p>
+        <h2 className="font-sb-aggro-medium text-heading-4 text-label-normal md:text-heading-3">{title}</h2>
         {updateTime && <p className="text-label-regular text-label-alternative">{updateTime}전 업데이트</p>}
       </article>
       {prevElId && nextElId && (
