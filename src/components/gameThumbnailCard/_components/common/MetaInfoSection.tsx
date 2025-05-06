c80d38cdd82dde269e4007848241e2597a782fad
KoJaem <woalswhwh@naver.com>
2025-05-06 22:51:22 +0900

    chore: image fallback 처리

--- src/components/gameThumbnailCard/_components/common/MetaInfoSection.tsx
+++ src/components/gameThumbnailCard/_components/common/MetaInfoSection.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { useState } from "react"
 
 interface Params {
   creatorNickname?: string
@@ -6,17 +7,21 @@ interface Params {
 }
 
 export default function MetaInfoSection({ creatorNickname, creatorImage }: Params) {
+  const [errorFallbackURL, setErrorFallbackURL] = useState<string>()
   return (
     <article className="flex flex-col justify-between lg:flex-row">
       <div className="flex items-center gap-[4px]">
         <Image
-          src={creatorImage ?? "/images/Rookeys.png"}
+          src={(errorFallbackURL || creatorImage) ?? "/"}
           width={24}
           height={24}
           className="h-[24px] w-[24px] rounded-full"
           alt="creator-image"
           // placeholder="blur"
           // blurDataURL="data:image/jepg;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO88B8AAqUB0Y/H4mkAAAAASUVORK5CYII="
+          onError={() => {
+            setErrorFallbackURL("/images/Rookeys.png")
+          }}
         />
         <p className="line-clamp-1 text-label-regular text-label-neutral">{creatorNickname}</p>
       </div>
