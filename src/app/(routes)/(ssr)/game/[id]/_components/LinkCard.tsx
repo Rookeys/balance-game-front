60278ee361453fd508a46cf2cba72336ab3c8fba
KoJaem <woalswhwh@naver.com>
2025-05-21 22:40:12 +0900

    chore: LinkCard 배경색상 수정

--- src/app/(routes)/(ssr)/game/[id]/_components/LinkCard.tsx
+++ src/app/(routes)/(ssr)/game/[id]/_components/LinkCard.tsx
@@ -10,8 +10,11 @@ interface Params {
 
 export default function LinkCard({ title, image, alt, href }: Params) {
   return (
-    <Link href={href} className="flex w-full cursor-pointer flex-col rounded-[12px] border bg-gray-100 p-[16px]">
-      <p className="text-body2-bold">{title}</p>
+    <Link
+      href={href}
+      className="transition-color-custom flex w-full cursor-pointer flex-col rounded-[12px] bg-fill-normal p-[16px] hover:bg-fill-strong"
+    >
+      <p className="text-label-bold md:text-body2-bold">{title}</p>
       <Image src={image} width={60} height={60} alt={alt} className="self-end" />
     </Link>
   )
