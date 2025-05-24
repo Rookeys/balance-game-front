78b14acd36c22699ae6725ca62f5d96fa98fad00
KoJaem <woalswhwh@naver.com>
2025-05-25 02:04:46 +0900

    chore: 에러페이지 문구 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -15,7 +15,11 @@ export default function ErrorPage() {
         <article className="flex flex-col items-center gap-[12px]">
           <Image src={"/images/character/pixy_error.webp"} alt="error-image" width={80} height={80} />
           <p className="font-sb-aggro-medium text-heading-5 md:text-heading-4">이런, 오류가 발생했군요</p>
-          <p className="text-label-regular text-label-neutral md:text-body2-regular">잠시 후 다시 시도해 주세요.</p>
+          <p className="text-center text-label-regular text-label-neutral md:text-body2-regular">
+            잠시 후 다시 시도해 주세요.
+            <br />
+            잘못된 페이지 또는 서버가 원활하지 않습니다.
+          </p>
         </article>
         <Button
           onClick={() => {
