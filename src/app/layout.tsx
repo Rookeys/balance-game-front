076f1b9a25d0bfbafdd5540e3ff89bd5de9cb64d
KoJaem <woalswhwh@naver.com>
2025-05-27 19:03:26 +0900

    chore: canonical URL 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default async function RootLayout({
       <head>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="canonical" href="https://zznpk.com" />
       </head>
       <body
         className={cn(
