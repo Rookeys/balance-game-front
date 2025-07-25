ded46a769da6eb3c116631243efaee372f443488
KoJaem <woalswhwh@naver.com>
2025-07-26 04:39:26 +0900

    chore: 모바일환경 월드컵생성 버튼 높이 수정 #74

--- src/components/form/gameRoom/_components/GameFormBottomBar.tsx
+++ src/components/form/gameRoom/_components/GameFormBottomBar.tsx
@@ -39,6 +39,7 @@ export default function GameFormBottomBar({ step, setStep, percent = 0, disabled
         data-tooltip-id={`game-create-bottom-bar-button-${step}`}
         key={`${step}-button`}
         type={isMediasPage || step === 1 ? "button" : "submit"}
+        className="h-full"
         disabled={disabled}
         onClick={async () => {
           if (isMediasPage || isResourcesPage) {
